3a2ae32a97c2e124744245477d19fca1f425b34c
larakoseff <larakoseff@gmail.com>
2023-08-11 10:36:21 +0200

    Complete Challenge 1

---
+++ challenge1/scripts.js
@@ -0,0 +1,6 @@
+const value = 3
+console.log(value + 4 + value)
+
+
+// 343 was logged initially as the value was declared as a string and the
+// numbers were concatenated instead of added together. 
